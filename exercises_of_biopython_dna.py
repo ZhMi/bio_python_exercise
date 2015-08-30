4808c4dcae191916bfd5a3ea39867b2ef9df9003
zhmi <zmmingtiandege1314@126.com>
2015-08-30 22:19:26 +0800

    Adding description about the problem which this program will solve .Simplifying codes again.

--- exercises_of_biopython_dna.py
+++ exercises_of_biopython_dna.py
@@ -4,7 +4,17 @@
 ####################################### PART0 Description ##############################################################
 
 # Filename:        exercises_of_biopython_dna.py
-# Description:     doing some exercises about biopython
+# Description:     doing some exercises about bio_python
+#                  dna.fasta is a txt of DNA sequence
+#                  A record in a dna.fasta file is defined as a single-line header, followed by lines of sequence data.
+#                  The header line is distinguished from the sequence data by a greater-than (">") symbol in the first
+#                  column. The word following the ">" symbol is the identifier of the sequence, and the rest of the line
+#                  is an optional description of the entry. There should be no space between the ">" and the first
+#                  letter of the identifier.  An open reading frame (ORF) is the part of a reading frame that has the
+#                  potential to encode a protein. It starts with a start codon (ATG), and ends with a stop codon
+#                  (TAA, TAG or TGA). For instance, ATGAAATAG is an ORF of length 9.
+#                  This program is written to solve the question about"What is the length of the longest ORF appearing
+#                  in reading frame 1 or frame 2 or frame 3 of any of the sequences"
 # Author:          ZhMi
 # E-mail:          zmmingtiandege1314@126.com
 # Create:          2015-8-29
@@ -23,22 +33,6 @@ except:
 finally:
     fp.close()
 
-'''
-[the first vision]
-total_original_str = ''
-
-for i in xrange(len(original_list)):
-    original_list[i] = original_list[i].strip()
-    if original_list[i][0] == '>':
-       original_list[i] = '>'
-    total_original_str =  total_original_str + original_list[i]
-
-sequence_list = total_original_str.split('>')
-sequence_list = filter(lambda x:x!='',sequence_list)
-
-'''
-
-#[the second vision]
 total_original_str = ''
 def combine_sequence(line):
     global total_original_str
@@ -49,17 +43,6 @@ def combine_sequence(line):
 map(combine_sequence,original_list)
 sequence_list = total_original_str.split('>')
 sequence_list = filter(lambda x:x!='',sequence_list)
-
-'''
-[text codes]
-print "length of sequence_list:",len(sequence_list)
-length_sequence_list = map(len,sequence_list)
-print length_sequence_list
-'''
-
-#sepratelist = filter(lambda x:x[0:3]=='ATG',sepratelist)
-#sepratelist = filter(lambda x:x[-3:]=='TAG' or x[-3:]=='TAA'or x[-3:]=='TGA',sepratelist)
-#three_dna_sequence_list = []
 
 def seperate_three_sequence(line,read_frame):
     templist = []
@@ -72,22 +55,7 @@ def seperate_three_sequence(line,read_frame):
     return templist
 
 frame_input = input("Please input the reading frame of sequence:")
-'''
-#[the first  vision]
-frame_list = [frame_input for i in range(len(sequence_list))]
-three_dna_sequence_list = map(seperate_three_sequence,sequence_list,frame_list)
-'''
-#[the second vision]
 three_dna_sequence_list = map(lambda element_in_list: seperate_three_sequence(element_in_list, frame_input), sequence_list)
-
-'''
-print 'length of three_dna_sequence_list:',len(three_dna_sequence_list)
-[test codes]
-for i in xrange(3):
-    print "******-----*******"
-    print three_dna_sequence_list[i]
-    print '\n'
-'''
 
 def end_code_position_find(end_code_list,sequence):
     try:
@@ -104,7 +72,6 @@ def end_code_position_find(end_code_list,sequence):
         pass
     return end_code_list
 
-
 def ofr_sequence_find(line):
     templist = []
     while(len(line)):
@@ -116,23 +83,6 @@ def ofr_sequence_find(line):
             break
         else:
             line = line[begin_code_pos:]
-            '''
-            [the first vision]
-            end_code_pos_list = []
-            try:
-                end_code_pos_list.append(line.index('TAG'))
-            except:
-                pass
-            try:
-                end_code_pos_list.append(line.index('TAA'))
-            except:
-                pass
-            try:
-                end_code_pos_list.append(line.index('TGA'))
-            except:
-                pass
-            '''
-            #[the second vision]
             end_code_pos_list = []
             end_code_position_find(end_code_pos_list,line)
             if len(end_code_pos_list)==0 :
@@ -145,13 +95,7 @@ def ofr_sequence_find(line):
     return templist
 
 ofr_sequence_list = map(ofr_sequence_find,three_dna_sequence_list)
-
-#[text codes]
-print 'befor filter action,length of ofr_sequence_list:',len(ofr_sequence_list)
-
 ofr_sequence_list = filter(lambda x:len(x)!=0,ofr_sequence_list)
-#[text codes]
-print 'after filter action,length of ofr_sequence_list:',len(ofr_sequence_list)
 
 '''
 [example about flatten]
@@ -160,16 +104,8 @@ a = [[1,3],[2,4],[3,5],["abc","def"]]
 >>> a1[1,3,2,4,3,5,"abc","def"]
 '''
 
-#[flatten action]
-ofr_sequence_list = [y for x in ofr_sequence_list for y in x]
-
-#[text codes]
-print 'after flatten action,length of ofr_sequence_list:',len(ofr_sequence_list)
-
+ofr_sequence_list = [y for x in ofr_sequence_list for y in x] #[flatten action]
 ofr_sequence_length_list = map(len,ofr_sequence_list)
-
-print 'after get length action,length of ofr_sequence_ength_list',len(ofr_sequence_length_list)
-
 max_ofr_sequence_length = 3*max(ofr_sequence_length_list)
 print "max length of max_ofr_sequence_length:",max_ofr_sequence_length
 
