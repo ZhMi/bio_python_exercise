77ab392c005d64273678a7e127a22c78a0c2aabe
zhmi <zmmingtiandege1314@126.com>
2015-08-30 14:55:11 +0800

    find  the length of OFR in all the sequences,use ofr_sequence_length_list store all the length of OFR sequence

--- exercises_of_biopython_dna.py
+++ exercises_of_biopython_dna.py
@@ -3,11 +3,12 @@
 
 ####################################### PART0 Description ##############################################################
 
-# Filename:     exercises_of_biopython_dna.py
-# Description : doing some exercises about biopython
-# Author:       ZhMi
-# E-mail:       zmmingtiandege1314@126.com
-# Create:       2015-8-29
+# Filename:        exercises_of_biopython_dna.py
+# Description:     doing some exercises about biopython
+# Author:          ZhMi
+# E-mail:          zmmingtiandege1314@126.com
+# Create:          2015-8-29
+# Recent-changes:  2015-8-30
 
 ####################################### Part1 : coding    ##############################################################
 fp = open("/home/zhmi/Documents/dna2.fasta")
@@ -31,6 +32,13 @@ for i in xrange(len(original_list)):
 
 sequence_list = total_original_str.split('>')
 sequence_list = filter(lambda x:x!='',sequence_list)
+
+'''
+[text codes]
+print "length of sequence_list:",len(sequence_list)
+length_sequence_list = map(len,sequence_list)
+print length_sequence_list
+'''
 
 #sepratelist = filter(lambda x:x[0:3]=='ATG',sepratelist)
 #sepratelist = filter(lambda x:x[-3:]=='TAG' or x[-3:]=='TAA'or x[-3:]=='TGA',sepratelist)
@@ -64,7 +72,7 @@ for i in xrange(3):
 def ofr_sequence_find(line):
     templist = []
     line = line[1:]
-    while(len(line)>=6):
+    while(len(line)):
         try:
             begin_code_pos = line.index('ATG')
         except:
@@ -86,12 +94,11 @@ def ofr_sequence_find(line):
                 end_code_pos_list.append(line.index('TGA'))
             except:
                 pass
-            #end_code_pos_list = filter(lambda x:x!='',end_code_pos_list)
             if len(end_code_pos_list)==0 :
                 break
             else:
                 end_code_pos = min(end_code_pos_list)
-                templine = line[begin_code_pos-1:end_code_pos+1]
+                templine = line[:end_code_pos+1]
                 templist.append(templine)
                 line = line[end_code_pos+1:]
     return templist
@@ -99,18 +106,29 @@ def ofr_sequence_find(line):
 ofr_sequence_list = map(ofr_sequence_find,three_dna_sequence_list)
 
 #[text codes]
-print 'length of ofr_sequence_list:',len(ofr_sequence_list)
-for i in xrange(3):
-    print i,ofr_sequence_list[i]
-    print '\n'
+print 'befor filter action,length of ofr_sequence_list:',len(ofr_sequence_list)
 
+ofr_sequence_list = filter(lambda x:len(x)!=0,ofr_sequence_list)
+#[text codes]
+print 'after filter action,length of ofr_sequence_list:',len(ofr_sequence_list)
+'''
+[example about flatten]
+a = [[1,3],[2,4],[3,5],["abc","def"]]
+>>> a1 = [y for x in a for y in x]
+>>> a1[1,3,2,4,3,5,"abc","def"]
+'''
+ofr_sequence_list = [y for x in ofr_sequence_list for y in x]
 
+#[text codes]
+print 'after flatten action,length of ofr_sequence_list:',len(ofr_sequence_list)
 
+ofr_sequence_length_list = map(lambda x:len(x),ofr_sequence_list)
 
-
-
-
-
+print 'after get length action,length of ofr_sequence_ength_list',len(ofr_sequence_length_list)
+'''
+for i in ofr_sequence_list:
+    print i
+'''
 
 
 
